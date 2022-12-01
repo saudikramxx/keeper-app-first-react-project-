da91f7543e276e288ca9a6ae467dd248fca03ac0
saud ikram <116587678+saudikramxx@users.noreply.github.com>
2022-12-01 18:53:10 +0530

    added header footer and note

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./Footer";
+import Note from "./note";
+
+
+function App(){
+    return <div><Header/><Note/><Footer/></div>
+}
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+var year = new Date().getFullYear()
+
+function Footer(){
+    return <footer><p>copyright {year}</p></footer>
+}
+export default Footer;
